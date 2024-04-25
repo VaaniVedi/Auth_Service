aa4b324ac2e6fab0a2f52f5ecefd1a6b57ae416b
Vani <workwithvanichaturvedi@gmail.com>
2024-04-25 10:26:25 +0530

    Added get user by ID

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ const { PORT } = require('./config/serverConfig')
 const apiRoutes = require('./routes/index');
 
 /*const {User} = require('./models/index');
-const bcrypt = require('bcrypt');*/
+const bcrypt = require('bcrypt');
+const UserRepository = require('./repository/user-repository')*/
 
 const app=express();
 
@@ -18,7 +19,10 @@ const prepareAndStartServer = () => {
 
     app.listen(PORT, async() => {
         console.log(`Server Started on Port: ${PORT}`);
-        /*const incomingPassword = '123556';
+        /*const repo = new UserRepository()
+        const response = await repo.getById(1);
+        console.log(response)
+        const incomingPassword = '123556';
         const user = await User.findByPk(3)
         const response = bcrypt.compareSync(incomingPassword,user.password);
         console.log(response);*/
