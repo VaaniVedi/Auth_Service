759ba47987be243a0dcaf882571cff7d54713b2a
Vani <workwithvanichaturvedi@gmail.com>
2024-05-03 20:09:57 +0530

    Error handling using http-status-codes

--- src/repository/user-repository.js
+++ src/repository/user-repository.js
@@ -1,5 +1,7 @@
 const ValidationError = require('../utils/ValidationError');
 const { User,Role } = require('../models/index');
+const ClientError = require('../utils/client-error');
+const {StatusCodes} = require('http-status-codes');
 
 class UserRepository {
 
@@ -47,8 +49,17 @@ class UserRepository {
             const user=await User.findOne({where: {
                 email: userEmail
             }});
+            if(!user){
+                throw new ClientError (
+                    'AttributeNotFound',
+                    'Invalid email sent in the request',
+                    'Please check the email, as there is no record of the email',
+                    StatusCodes.NOT_FOUND
+                )
+            }
             return user;
         } catch (error) {
+            console.log(error)
             console.log("Something went wrong on repository layer");
             throw error;
         }
